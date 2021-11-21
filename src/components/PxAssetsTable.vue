334122a25f11fb68c85f9c0299f0c5e38d2f21ee
David Granados <david.granados@avaldigitallabs.com>
2021-11-21 12:25:55 -0500

    Mejorar proyecto: filtros y ordenar 36/38

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -3,20 +3,40 @@
     <thead>
       <tr class="bg-gray-100 border-b-2 border-gray-400">
         <th></th>
-        <th>
-          <span>Ranking</span>
+        <th :class="{ up: this.sortOrder === 1, down: this.sortOrder === -1 }">
+          <span @click="changeSortOrder" class="underline cursor-pointer">
+            Ranking
+          </span>
         </th>
         <th>Nombre</th>
         <th>Precio USD</th>
         <th>Cap. de Mercado</th>
         <th>Variación 24hs</th>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <input
+            class="
+              bg-gray-100
+              focus:outline-none
+              border-b border-gray-400
+              py-2
+              px-4
+              block
+              w-full
+              appearance-none
+              leading-normal
+            "
+            id="filter"
+            placeholder="Buscar..."
+            type="text"
+            v-model="filter"
+          />
+        </td>
       </tr>
     </thead>
     <tbody>
       <tr
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
-        v-for="asset in assets"
+        v-for="asset in filteredAssets"
         :key="asset.id"
       >
         <td>
@@ -72,6 +92,31 @@ export default {
     },
   },
 
+  data: () => {
+    return {
+      filter: '',
+      sortOrder: 1,
+    }
+  },
+
+  computed: {
+    filteredAssets() {
+      const altOrder = this.sortOrder === 1 ? -1 : 1
+      return this.assets
+        .filter(
+          (asset) =>
+            asset.symbol.toLowerCase().includes(this.filter.toLowerCase()) ||
+            asset.name.toLowerCase().includes(this.filter.toLowerCase())
+        )
+        .sort((a, b) => {
+          if (parseInt(a.rank) > parseInt(b.rank)) {
+            return this.sortOrder
+          }
+          return altOrder
+        })
+    },
+  },
+
   methods: {
     getPercentColorClass(asset) {
       return asset.changePercent24Hr.includes('-')
@@ -83,6 +128,9 @@ export default {
     },
     handleDetailButtonClick(id) {
       this.$router.push({ name: 'coin-detail', params: { id } })
+    },
+    changeSortOrder() {
+      return (this.sortOrder = this.sortOrder === 1 ? -1 : 1)
     },
   },
 }
